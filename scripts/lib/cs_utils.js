0ba23cb9088a36e2c8eab608fb52ab2b58c2e5e5
Pranay Anchuri <panchuri@princeton.edu>
2019-11-05 18:54:26 -0500

    adding cs utils

---
+++ scripts/lib/cs_utils.js
@@ -0,0 +1,16 @@
+
+// utils for content scripts
+
+function decode(url) {
+    var u = new URL(url);
+    // this is for facebook posts
+    hasU = u.searchParams.get('u') != null;
+    if(hasU) {
+        return u.searchParams.get("u").split('?')[0];
+    }
+    return u.href;
+}
+
+function getDomain(url) {
+    return new URL(url).hostname;
+}
