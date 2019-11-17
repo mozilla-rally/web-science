4ab67ea92d9970b250d7a752dd2199b6b9eb7655
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2019-11-16 19:19:35 -0500

    (split)Rearchitected the extension and rebuilt the navigation and attention tracking

---
+++ WebScience.js
@@ -0,0 +1,27 @@
+import * as Navigation from "/WebScience/study-modules/Navigation.js"
+export { Navigation }
+
+/* Module Variables */
+
+var studySpecificConsentRequired = true;
+
+const debug = true;
+
+/*  Consent - components related to checking for and obtaining user consent. */
+
+export function disableStudySpecificConsent() {
+  studySpecificConsentRequired = false;
+}
+
+// TODO implement logic for pausing studies until the user consents, if consent
+// is needed for a study
+export function requestConsent() {
+  browser.runtime.openOptionsPage();
+}
+
+/* Utilities */
+
+function debugLog(text) {
+  if(debug == true)
+    console.log(text);
+}
