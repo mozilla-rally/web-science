ceed66678032e3bee344f2ad7665857ecd36d39e
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2019-11-18 10:25:08 -0500

    Adding function encapsulation to content scripts and more notes on link exposure

--- WebScience/content-scripts/referrer.js
+++ WebScience/content-scripts/referrer.js
@@ -1,6 +1,11 @@
+// Function encapsulation to maintain unique variable scope for each content script
+(function() {
+
 browser.runtime.sendMessage({
   type: "WebScience.referrerUpdate",
   content: {
     referrer: document.referrer
   }
 });
+
+})();
