a0d6b6b5d793c576488cd623bae5b9cccaa703e1
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2019-11-20 17:01:55 -0500

    Adding stubs for the Utilities module

---
+++ WebScience/Utilities/Utilities.js
@@ -0,0 +1,11 @@
+import * as Storage from "/WebScience/Utilities/Storage.js"
+export { Storage }
+
+import * as Consent from "/WebScience/Utilities/Consent.js"
+export { Consent }
+
+import * as PageVisit from "/WebScience/Utilities/PageVisit.js"
+export { PageVisit }
+
+import * as PageAttention from "/WebScience/Utilities/PageAttention.js"
+export { PageAttention }
