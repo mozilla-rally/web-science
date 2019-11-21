2cdf33dccc3e34116cf2dd966209230c489e8452
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2019-11-20 20:27:56 -0500

    (split)Refactoring consent into a module

--- WebScience.js
+++ WebScience.js
@@ -3,19 +3,3 @@ export { Studies }
 
 import * as Utilities from "/WebScience/Utilities/Utilities.js"
 export { Utilities }
-
-/* Module Variables */
-
-var studySpecificConsentRequired = true;
-
-/*  Consent - components related to checking for and obtaining user consent. */
-
-export function disableStudySpecificConsent() {
-  studySpecificConsentRequired = false;
-}
-
-// TODO implement logic for pausing studies until the user consents, if consent
-// is needed for a study
-export function requestConsent() {
-  browser.runtime.openOptionsPage();
-}
