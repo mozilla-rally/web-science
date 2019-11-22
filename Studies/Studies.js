32cac66b8457dad1552ecf8194742ae6f3afe24f
Pranay Anchuri <panchuri@princeton.edu>
2019-11-22 13:46:15 -0500

    (split)adding social media new exposure study

--- Studies/Studies.js
+++ Studies/Studies.js
@@ -4,5 +4,8 @@ export { Navigation }
 import * as LinkExposure from "/WebScience/Studies/LinkExposure.js"
 export { LinkExposure }
 
+import * as SocialMediaLinkExposure from "/WebScience/Studies/SocialMediaLinkExposure.js"
+export { SocialMediaLinkExposure }
+
 import * as SocialMediaSharing from "/WebScience/Studies/SocialMediaSharing.js"
 export { SocialMediaSharing }
