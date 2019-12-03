27b9c4fcc58023a3472a27205e1a0ce3b5e6eb47
Anne Kohlbrenner <akohlbre@andrew.cmu.edu>
2019-12-03 17:47:19 -0500

    record likes of tweets with links in tweet or original, if retweet

--- WebScience/Studies/SocialMediaSharing.js
+++ WebScience/Studies/SocialMediaSharing.js
@@ -4,7 +4,7 @@ const debugLog = WebScience.Utilities.Debugging.getDebuggingLog("SocialMediaShar
 /*  SocialMediaSharing - This module is used to run studies that track the user's
     social media sharing of links. */
 
-    var storage = null;
+var storage = null;
 
 /*  runStudy - Starts a SocialMediaSharing study. Note that only one study is supported
     per extension. runStudy requires an options object with the following
@@ -15,7 +15,7 @@ const debugLog = WebScience.Utilities.Debugging.getDebuggingLog("SocialMediaShar
         * reddit - whether to track URL shares on Reddit (default false) */
 
 export async function runStudy({
-    domains = [ ],
+    domains = [],
     facebook = false,
     twitter = false,
     reddit = false
@@ -38,17 +38,26 @@ export async function runStudy({
     // how long the user spent on the page
 
     // Twitter
-    if(twitter) {
+    if (twitter) {
         // If the user POSTS a status update, parse it for matching URLs
-        /* Note that there's 'url' field that gets set if the user
-         *  shares from, for example, a news article. However, if the user
-         *  doesn't also include the url in the body of the tweet, it won't
-         *  be shown in the tweet as it displays to others, so I'm not
-         *  considering the 'url' field when deciding whether a user tweeted
-         *  an article, just urls in the body of the tweet.
+        /* When the user tweets from the share button on a website, there's a
+         *  'url' field in the request that has the url of the story in it.
+         * However, the user also gets a chance to edit the pre-written tweet,
+         *  which initially includes the headline and link. If they remove the
+         *  link from the body of the tweet, Twitter seems to ignore the
+         *  'url' field when displaying the tweet (ie, no news box is shown).
+         * Therefore, we're ignoring the 'url' field when deciding whether
+         *  the user has tweeted a link, and just looking at the body of the post.
+         */
+        /* Also, for considering exposure: when a link *ends* a tweet
+         *  (eg: "Here's a news article! www.example.com")
+         *  the link itself is not displayed as part of the tweet, but a news
+         *  box is still shown. When there is text after the link,
+         *  (eg "This www.example.com is a link")
+         *  the link itself is displayed *and* a news box is shown.
          */
         browser.webRequest.onBeforeRequest.addListener(async (requestDetails) => {
-            if(requestDetails.method != "POST")
+            if (requestDetails.method != "POST")
                 return;
 
             // Check that this is a recognizable status update (i.e., tweet) request
@@ -74,22 +83,87 @@ export async function runStudy({
             }
 
         },
-        // Using a wildcard for the API version in case that changes
-        { urls: [
-            "https://api.twitter.com/*/statuses/update.json", /* catches tweets made from twitter.com */
-            "https://twitter.com/intent/tweet", /* catches tweets made via share links on websites */
+            // Using a wildcard for the API version in case that changes
+            {
+                urls: [
+                    "https://api.twitter.com/*/statuses/update.json", /* catches tweets made from twitter.com */
+                    "https://twitter.com/intent/tweet", /* catches tweets made via share links on websites */
                 ]
-        }, 
-        [ "requestBody" ]);
+            },
+            ["requestBody"]);
 
         // TODO handle retweets
         // Looks like the relevant API endpoint is https://api.twitter.com/1.1/statuses/retweet.json
         // Will likely have to resolve the tweet getting retweeted with another API call
         // to the endpoint at https://api.twitter.com/1.1/statuses/lookup.json
 
-        // TODO likes
-        // Looks like the relevant API enpoint is https://api.twitter.com/1.1/favorites/create.json
-        // Will have to resolve the tweet getting liked for any URLs
+        // Handle Twitter likes
+        browser.webRequest.onBeforeRequest.addListener((requestDetails) => {
+            if (requestDetails.method != "POST")
+                return;
+
+            // Check that this is a recognizable favorite request
+            if ((requestDetails.requestBody == null) ||
+                !("formData" in requestDetails.requestBody) ||
+                !("id" in requestDetails.requestBody.formData) ||
+                (requestDetails.requestBody.formData["id"].length == 0))
+                return;
+
+            var favoriteTime = Date.now();
+
+            var requestId = requestDetails.requestId;
+
+            /* The response to the POST request for the favorite contains details about the tweet that was liked.
+             * Unfortunately, that data is in the *body* of the response, not the headers, so it's trickier to get.
+             * Our ResponseBody library listens for and collects together the response, and calls the 
+             *  listener below once the response body comes through.
+             */
+            WebScience.Utilities.ResponseBody.registerResponseBodyListener(requestId, async (responseContents) => {
+
+                var expanded_urls = [];
+                // grab all the urls in the liked tweet itself
+                if (("entities" in responseContents &&
+                    "urls" in responseContents.entities)) {
+                    for (var urlObject of responseContents.entities.urls) {
+                        if ("expanded_url" in urlObject) {
+                            expanded_urls.push(urlObject.expanded_url);
+                        }
+                    }
+                }
+
+                // if this is a retweet, grab all the urls in the original tweet
+                if (("retweeted_status" in responseContents &&
+                    "entities" in responseContents.retweeted_status &&
+                    "urls" in responseContents.retweeted_status.entities)) {
+                    for (var urlObject of responseContents.retweeted_status.entities.urls) {
+                        if ("expanded_url" in urlObject) {
+                            expanded_urls.push(urlObject.expanded_url)
+                        }
+                    }
+                }
+
+                /* TODO: probably I don't understand Twitter well enough, but sometimes the urls are
+                 *  duplicated because they appear in the tweet and the retweet? Even when it doesn't
+                 *  look like they're in both. Anyway, we should de-duplicate that.
+                 */
+                // check whether the found urls are ones we care about and report if so
+                for (var expanded_url of expanded_urls) {
+                    if (urlMatcher.testUrl(expanded_url)) {
+                        var shareRecord = createShareRecord(favoriteTime, "twitter", expanded_url, "favorite");
+                        storage.set((await shareIdCounter.getAndIncrement()).toString(), shareRecord);
+                        debugLog("Twitter favorite: " + JSON.stringify(shareRecord));
+                    }
+                }
+            }, true); // pass true to tell ResponseBody to json parse the reply for us
+
+        },
+            // Using a wildcard for the API version in case that changes
+            {
+                urls: [
+                    "https://api.twitter.com/*/favorites/create.json", /* catches likes made from twitter.com */
+                ]
+            },
+            ["requestBody", "blocking"]);
 
         // TODO handle quote tweets
         // Looks like quoted tweets are referenced in the attachment_url property using
@@ -98,7 +172,7 @@ export async function runStudy({
     }
 
     // Facebook
-    if(facebook) {
+    if (facebook) {
         // TODO implement post support
         // Looks like the relevant API endpoint is https://www.facebook.com/webgraphql/mutation/?doc_id=...
         // Will have to check the form body's variables.input.message.text (for post
@@ -108,10 +182,10 @@ export async function runStudy({
     }
 
     // Reddit
-    if(reddit) {
+    if (reddit) {
         // If the user POSTs a new post, parse it for matching URLs
         browser.webRequest.onBeforeRequest.addListener(async (requestDetails) => {
-            if(requestDetails.method != "POST")
+            if (requestDetails.method != "POST")
                 return;
 
             // Check that this is a recognizable post
@@ -121,10 +195,10 @@ export async function runStudy({
             var shareTime = Date.now();
 
             // Handle if there's a URL attached to the post
-            if(("url" in requestDetails.requestBody.formData) &&
+            if (("url" in requestDetails.requestBody.formData) &&
                 (requestDetails.requestBody.formData["url"].length == 1)) {
                 var postUrl = requestDetails.requestBody.formData["url"][0];
-                if(urlMatcher.testUrl(postUrl)) {
+                if (urlMatcher.testUrl(postUrl)) {
                     var shareRecord = createShareRecord(shareTime, "reddit", postUrl, "post");
                     storage.set((await shareIdCounter.getAndIncrement()).toString(), shareRecord);
                     debugLog("Reddit: " + JSON.stringify(shareRecord));
@@ -147,9 +221,9 @@ export async function runStudy({
             if (!("richtext_json" in requestDetails.requestBody.formData)) return;
             var postObject = JSON.parse(requestDetails.requestBody.formData["richtext_json"]);
             if (!("document" in postObject &&
-                  postObject.document.length > 0 &&
-                  "c" in postObject.document[0]))
-              return;
+                postObject.document.length > 0 &&
+                "c" in postObject.document[0]))
+                return;
 
             // Handle when there's a url in the post body
             var postBody = postObject.document[0].c;
@@ -164,10 +238,11 @@ export async function runStudy({
             }
 
         },
-        // Using a wildcard at the end of the URL because Reddit adds parameters
-        { urls: [ "https://oauth.reddit.com/api/submit*" ] }, 
-        [ "requestBody" ]);
+            // Using a wildcard at the end of the URL because Reddit adds parameters
+            { urls: ["https://oauth.reddit.com/api/submit*"] },
+            ["requestBody"]);
     }
+
 }
 
 /* Utilities */
@@ -183,7 +258,7 @@ function createShareRecord(shareTime, platform, url, event) {
 
 // Helper function that dumps the navigation study data as an object
 export async function getStudyDataAsObject() {
-    if(storage != null)
+    if (storage != null)
         return await storage.getContentsAsObject();
     return null;
 }
