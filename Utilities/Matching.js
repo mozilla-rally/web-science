0d79402007335524f9a84ff5b4f4f978ad5bebec
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2019-12-22 17:34:16 -0500

    (split)JSDoc markup for the Matching module #50

--- Utilities/Matching.js
+++ Utilities/Matching.js
@@ -1,18 +1,40 @@
-// Class for testing whether a URL matches an array of domains
-// Currently implemented with the native RegExp, which gives good performance
-// We might be able to speed this up by switching to a different implementation,
-// if needed (e.g., a trie or URL parsing and a set of domains)
+/**
+ * This module provides utilities for matching URLs against domain names.
+ * 
+ * @module WebScience.Utilities.Matching
+ */
+
+/** 
+ * Class for testing whether a URL matches a set of domains.
+ * Currently implemented with the native RegExp over the full URL, which gives good performance.
+ * We might be able to speed this up by parsing the URL and then only matching domains.
+ */
 export class UrlMatcher {
+    /**
+     * Create a URL matcher.
+     * @param {string[]} domains - The set of domains to match against.
+     * @param {boolean} [matchSubdomains=true] - Whether to match subdomains of domains in the set.
+     */
     constructor(domains, matchSubdomains = true) {
         this.regExp = new RegExp(createUrlRegexString(domains, matchSubdomains));
     }
 
+    /**
+     * Test whether a URL matches a domain in the set of domains.
+     * @param {string} url - The URL to test.
+     */
     testUrl(url) {
         return this.regExp.test(url);
     }
 }
 
-// Returns a regular expression string for matching URLs against an array of domains
+/**
+ * Generate a regular expression string for matching a URL against a set of domains.
+ * Will match http and https protocols. Currently case sensitive.
+ * @param {string[]} domains - The set of domains to match against.
+ * @param {boolean} [matchSubdomains=true] - Whether to match subdomains of domains in the set.
+ * @returns {string} A regular expression string.
+ */
 export function createUrlRegexString(domains, matchSubdomains = true) {
     var urlMatchRE = "^(?:http|https)://" + (matchSubdomains ? "(?:[A-Za-z0-9\\-]+\\.)*" : "") + "(?:";
     for (const domain of domains)
@@ -21,7 +43,13 @@ export function createUrlRegexString(domains, matchSubdomains = true) {
     return urlMatchRE;
 }
 
-// Returns an array of match patterns for matching URLs against an array of domains
+/**
+ * Generate an array of match patterns for matching a URL against a set of domains.
+ * Will match http and https protocols.
+ * @param {string[]} domains - The set of domains to match against.
+ * @param {boolean} [matchSubdomains=true] - Whether to match subdomains of domains in the set.
+ * @returns {string[]} An array of match patterns.
+ */
 export function createUrlMatchPatternArray(domains, matchSubdomains = true) {
     var matchPatterns = [ ];
     for (const domain of domains) {
