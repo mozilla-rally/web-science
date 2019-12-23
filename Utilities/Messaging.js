4c0f111ebdc4503c8e1dbe41af96d6abf5484bf8
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2019-12-23 00:20:05 -0500

    (split)JSDoc markup improvements for the Messaging module #50

--- Utilities/Messaging.js
+++ Utilities/Messaging.js
@@ -1,26 +1,45 @@
 /** 
- * This module provides a convenience wrapper around browser.runtime.onMessage,
+ * This module provides a convenience wrapper around `browser.runtime.onMessage`,
  * routing messages to the right listener(s) based on the message type.
  * Message types are defined as strings, and a message must be an object with a
- * type field to be handled correctly.
+ * type property to be handled correctly.
  * @module WebScience.Utilities.Messaging
  */
 
 import * as WebScience from "/WebScience/WebScience.js"
 const debugLog = WebScience.Utilities.Debugging.getDebuggingLog("Utilities.Messaging");
 
-// Message listeners are stored in a Map where the key is the message type and
-// the value is a Set of listeners.
+/**
+ * A Map that stores message listeners. The keys are message types and the values
+ * are Sets of message listeners.
+ * @private
+ * @const {Map<string,Set<function>}
+ */
 const messageRouter = new Map();
 
-// Message schemas are stored in a Map where the key is the message type and
-// the value is the schema.
+/**
+ * A Map that stores message schemas. The keys are message types and the values
+ * are schemas.
+ * @private
+ * @const {Map<string,Object>}
+ */
 const messageSchemas = new Map();
 
+/**
+ * Whether the module's `browser.runtime.onMessage` listener has been registered.
+ * @private
+ * @type {boolean}
+ * @default
+ */
 var initialized = false;
 
-// A listener for browser.runtime.onMessage that routes messages to the right
-// listener(s) based on message type.
+/**
+ * A listener for `browser.runtime.onMessage` that routes messages to the right
+ * listener(s) based on message type. See the documentation for `browser.runtime.onMessage`
+ * for detail on the parameters.
+ * @returns {Promise} - An optional response to the message.
+ * @private
+ */
 function browserRuntimeListener(message, sender, sendResponse) {
     var messageListeners;
     var messageSchema;
@@ -57,8 +76,8 @@ function browserRuntimeListener(message, sender, sendResponse) {
  * Registers a message listener.
  * @param {string} messageType - The type of message that triggers the listener function.
  * @param {function} messageListener - The listener function, which receives the same
- * parameters as if it had been called by browser.runtime.onMessage, and that can
- * return the same values as a listener to browser.runtime.onMessage.
+ * parameters as if it had been called by `browser.runtime.onMessage`, and that can
+ * return the same values as a listener to `browser.runtime.onMessage`.
  * @param {object} [messageSchema] - An optional schema to register for the message type.
  */
 export function registerListener(messageType, messageListener, messageSchema) {
@@ -81,8 +100,8 @@ export function registerListener(messageType, messageListener, messageSchema) {
 /**
  * Registers a message schema.
  * @param {string} messageType - The type of message that must follow the schema.
- * @param {object} messageSchema - An object where each field has a value that is
- * a type string.
+ * @param {Object} messageSchema - An object where each field has a value that is
+ * a built-in type string.
  */
 export function registerSchema(messageType, messageSchema) {
     if(messageSchemas.has(messageType))
