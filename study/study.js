3837966b2e6d4afb9db5bfcc6c1c66123fbd5ba4
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2019-12-26 16:56:02 -0600

    Removing the debugging browser action #44

--- study/study.js
+++ study/study.js
@@ -30,81 +30,63 @@ function stopStudies() {
 }
 
 function runStudies() {
-  debugLog("Beginning study");
-  // Configure navigation collection
-  WebScience.Studies.Navigation.runStudy({
-    domains: studyDomains,
-    trackUserAttention: true,
-    savePageContent: false
-  });
+    debugLog("Beginning study");
+    // Configure navigation collection
+    WebScience.Studies.Navigation.runStudy({
+        domains: studyDomains,
+        trackUserAttention: true,
+        savePageContent: false
+      });
 
-// Configure link exposure collection
-WebScience.Studies.LinkExposure.runStudy({
-  domains: studyDomains,
-  shortdomains: shortDomains
-});
+    // Configure link exposure collection
+    WebScience.Studies.LinkExposure.runStudy({
+        domains: studyDomains,
+        shortdomains: shortDomains
+    });
 
-// Configure link exposure collection
-/*WebScience.Studies.SocialMediaLinkExposure.runStudy({
-  domains: studyDomains,
-  shortdomains: shortDomains,
-  socialmedia: socialMedia
-});*/
+    // Configure link exposure collection
+    
+    // Configure social media sharing collection
+    /*
+    WebScience.Studies.SocialMediaSharing.runStudy({
+        domains: studyDomains,
+        facebook: true,
+        twitter: true,
+        reddit: true
+    });
+    */
+    
+    // TODO configure social media account exposure collection
+    // Something like...
+    
+    /*
+    WebScience.Studies.SocialMediaAccountExposure.runStudy({
+        facebookAccounts: [ ],
+        twitterAccounts: [ ],
+        youtubeAccounts: [ ]
+    });
+    */
+    
+    // TODO configure social media news exposure collection (i.e., content
+    // recognized by platforms as news regardless of whether we recognize the domain
+    // Something like...
+    
+    /*
+    WebScience.Studies.SocialMediaNewsExposure.runStudy({
+        facebook: true,
+        twitter: true,
+        youtube: true
+    });
+    */
 
-// Configure social media sharing collection
-/*WebScience.Studies.SocialMediaSharing.runStudy({
-  domains: studyDomains,
-  facebook: true,
-  twitter: true,
-  reddit: true
-});*/
+    // Configure surveys
+    // TODO something like...
 
-  // TODO configure social media account exposure collection
-  // Something like...
-
-  /*
-  WebScience.Studies.SocialMediaAccountExposure.runStudy({
-    facebookAccounts: [ ],
-    twitterAccounts: [ ],
-    youtubeAccounts: [ ]
-  });
-  */
-
-  // TODO configure social media news exposure collection (i.e., content
-  // recognized by platforms as news regardless of whether we recognize the domain
-  // Something like...
-
-  /*
-  WebScience.Studies.SocialMediaNewsExposure.runStudy({
-    facebook: true,
-    twitter: true,
-    youtube: true
-  });
-  */
-
-  // Configure surveys
-  // TODO something like...
-
-  /*
-  WebScience.Studies.UserSurvey.runStudy({
-    surveyPromptText: "foo",
-    surveyUrl: "bar",
-    surveyTimeAfterInitialRun: 12345
-  });
-  */
-
-  // Temporary support for dumping the navigation study data to a downloaded file
-  browser.browserAction.onClicked.addListener(async (tab) => {
-    var navigationStudyData = await WebScience.Studies.Navigation.getStudyDataAsObject();
-    var linkExposureStudyData = await WebScience.Studies.LinkExposure.getStudyDataAsObject();
-    var socialMediaSharingStudyData = await WebScience.Studies.SocialMediaSharing.getStudyDataAsObject();
-    var counterData = await WebScience.Utilities.Storage.Counter.getContentsAsObject();
-    var combinedStudyData = {
-      navigation: navigationStudyData,
-      linkExposure: linkExposureStudyData,
-      socialMediaSharing: socialMediaSharingStudyData,
-      counters: counterData
-    };
-    browser.downloads.download({ url: URL.createObjectURL(new Blob([JSON.stringify(combinedStudyData)], { type: "application/json" })) });
-  });
+    /*
+    WebScience.Studies.UserSurvey.runStudy({
+        surveyPromptText: "foo",
+        surveyUrl: "bar",
+        surveyTimeAfterInitialRun: 12345
+    });
+    */
 }
