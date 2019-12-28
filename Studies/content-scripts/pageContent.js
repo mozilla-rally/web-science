1715da77618a6eba9848e587a30aa72c33767ef9
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2019-12-27 22:02:59 -0600

    (split)Private window support for Navigation content scripts #45

--- Studies/content-scripts/pageContent.js
+++ Studies/content-scripts/pageContent.js
@@ -1,8 +1,16 @@
 // Function encapsulation to maintain unique variable scope for each content script
-(function() {
+(async function() {
+
+// If the Navigation study is configured to not measure in private windows, and
+// this is a private window, do nothing
+var privateWindowResults = await browser.storage.local.get("WebScience.Studies.Navigation.privateWindows");
+if(("WebScience.Studies.Navigation.privateWindows" in privateWindowResults)
+    && !privateWindowResults["WebScience.Studies.Navigation.privateWindows"]
+    && browser.extension.inIncognitoContext)
+    return;
 
 browser.runtime.sendMessage({
-  type: "WebScience.pageContentUpdate",
+  type: "WebScience.Studies.Navigation.pageContentUpdate",
   pageContent: document.documentElement.outerHTML
 });
 
