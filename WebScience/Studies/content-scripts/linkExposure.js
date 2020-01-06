4e337da869606880fe2900676ac89d31983b8c49
Pranay Anchuri <panchuri@princeton.edu>
2020-01-06 18:46:44 -0500

    avoids round trip to content script

--- WebScience/Studies/content-scripts/linkExposure.js
+++ WebScience/Studies/content-scripts/linkExposure.js
@@ -7,6 +7,10 @@
      * are exposed to in known domains
      */
     const updateInterval = 2000;
+    /**
+     * @const
+     * visibilityThreshold (number of milliseconds) is minimum number of milliseconds for link exposure
+     */
     const visibilityThreshold = 10000;
     const elementSizeCache = new Map();
     linkExposure();
@@ -53,22 +57,26 @@
        * @returns {void} Nothing
        */
       function sendMessageToBackground(type, data) {
-        if (data.length > 0) {
+        if (data) {
           browser.runtime.sendMessage({
             type: type,
             loadTime: initialLoadTime,
             visible: initialVisibility,
             url: document.location.href,
             referrer: document.referrer,
-            links: data,
+            link: data,
           });
         }
       }
 
       /**
+       * @typedef {Object} Match
+       * @property {string} url - normalized url
+       * @property {Boolean} isMatched - domain matches 
+       * 
        * Function takes an element, tests it for matches with link shorteners or domains of interest and
        * @param {Element} element - href to match for short links or domains of interest
-       * @returns {boolean} - true if the url matches domains
+       * @returns {Match} match true if the url matches domains
        */
       function matchUrl(element) {
         let url = rel_to_abs(element.href);
@@ -86,37 +94,41 @@
 
       /**
        * Function to look for new <a> elements that are in viewport
-       * @returns {int} number of new links in the viewport
        */
       function observeChanges() {
         // check the visibility state of document
         if (!isDocVisible()) {
           return;
         }
-        // Filter for elements that haven't been visited previously and observe them with intersection observer
-        let count = 0;
+        // Get <a> elements and either observe (for new elements) or send them to background script if visible for > threshold
         Array.from(document.body.getElementsByTagName("a")).filter(link => link.hasAttribute("href")).forEach(element => {
+          // if we haven't seen this <a> element
           if (!checkedElements.has(element)) {
             processElement(element);
           } else {
-            // element is present
             let status = checkedElements.get(element);
+            // if we have seen and the element is visible for atleast threshold milliseconds
             if (status.isVisibleAboveThreshold(visibilityThreshold) && !status.isIgnored()) {
-              // send this to background script
-              sendMessageToBackground("WebScience.linkExposure", [{
+              // send <a> element this to background script
+              sendMessageToBackground("WebScience.linkExposure", {
                 href: status.url,
                 size: getElementSize(element),
                 firstSeen: status.visibility,
                 duration: status.getDuration()
-              }]);
+              });
               observer.unobserve(element);
               status.setIgnore();
             }
           }
         });
-        return count;
-        }
+      }
 
+      /**
+       * i) Checks if the normalized url matches domain/short domains
+       * ii) Observe matching elements with the intersection observer
+       * @param {HTMLElement} element 
+       * @returns {void} Nothing
+       */
       function processElement(element) {
         const {url, isMatched} = matchUrl(element);
         if(!isMatched) {
@@ -128,47 +140,8 @@
         observer.observe(element);
       }
 
-      class ElementStatus {
-        constructor(url) {
-          this.url = url;
-          this.matched = false;
-          this.visibility = null;
-          this.visibleDuration = 0;
-          this.ignore = false;
-        }
 
-        isIgnored() {
-          return this.ignore;
-        }
-
-        setIgnore() {
-          this.ignore = true;
-        }
-
-        isMatched() {
-          return this.matched;
-        }
-
-        setMatched() {
-          this.matched = true;
-        }
-        isVisibleAboveThreshold(threshold) {
-          return this.visibility != null && (Date.now() >= this.visibility + threshold);
-        }
-        setVisibility() {
-          this.visibility = Date.now();
-        }
-
-        getDuration() {
-          return Date.now() - this.visibility;
-        }
-        setDuration() {
-          if(this.visibility != null) {
-            this.visibleDuration = Date.now() - this.visibility;
-          }
-        }
-      }
-
+      /** callback for IntersectionObserver */
       function handleIntersection(entries, observer) {
         entries.forEach(entry => {
           const {
@@ -203,17 +176,13 @@
          * @param {int} numUpdates - maximum number of updates. ** Negative number implies function doesn't stop
          * @param {int} nrecords - maximum number of results stored
          */
-        constructor(updateInterval, numUpdates, nrecords = 10) {
+        constructor(updateInterval, numUpdates) {
           /** @member {int} - number of milliseconds */
           this.updateInterval = updateInterval;
           /** @member {int} - maximum number of updates or unlimited if negative */
           this.numUpdates = numUpdates;
           /** @member {int} - Number of times update has run */
           this.count = 0;
-          /** @member {Array} - Number of links discovered in each run */
-          this.nlinks = [];
-          /** @member {int} - History length to maintain */
-          this.nrecords = nrecords;
         }
         /**
          * calls the run method every @see run
@@ -238,11 +207,7 @@
           if (this.numUpdates > 0 && this.count >= this.numUpdates) {
             this.stop();
           }
-          let nchanges = observeChanges();
-          if (this.nlinks.length >= this.nrecords) {
-            this.nlinks.shift();
-          }
-          this.nlinks.push(nchanges);
+          observeChanges();
           this.count++;
         }
       }
@@ -250,37 +215,6 @@
       let handler = new UpdateHandler(updateInterval, -1);
       handler.start();
 
-      /**
-       * @function
-       * callback function for link resolution response messages from background script
-       * Each message is an object containing source and dest is the url after resolution
-       * @param {Object} message - object containing source and dest fields
-       * @param {*} sender - sender of the message
-       * @returns {void} Nothing
-       */
-      function listenerForLinkResolutionResponse(message, sender) {
-        let source = message.source;
-        let dest = message.dest;
-        if (urlMatcher.test(dest)) {
-          // get source size
-          let sz = getLinkSize(source);
-          if (sz == null) {
-            // check in cache
-            sz = elementSizeCache.has(source) ? elementSizeCache.get(source) : null;
-            // remove the url from cache
-            elementSizeCache.delete(source);
-          }
-          let data = [{
-            href: dest,
-            size: sz
-          }];
-          sendMessageToBackground("WebScience.linkExposure", data);
-        }
-        return Promise.resolve({
-          response: "cs received messages"
-        });
-      }
-      browser.runtime.onMessage.addListener(listenerForLinkResolutionResponse);
     } // End of link exposure function
   } // end of anon function
 )(); // encapsulate and invoke
