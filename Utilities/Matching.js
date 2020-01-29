cf1637c15037820627b210c91d6ee1df79a30adb
Anne Kohlbrenner <akohlbre@andrew.cmu.edu>
2020-01-29 16:23:01 -0500

    (split)check history and Navigation when saving sharing event

--- Utilities/Matching.js
+++ Utilities/Matching.js
@@ -58,3 +58,46 @@ export function createUrlMatchPatternArray(domains, matchSubdomains = true) {
     }
     return matchPatterns;
 }
+
+/**
+ * Remove url parameters from a given url.
+ * @param {string} url - the url to normalize
+ * @returns {string} - url with parameters stripped
+ */
+export function removeUrlParams(url) {
+    if (url.includes("?")) {
+        return url.substring(0, url.indexOf("?"));
+    }
+    return url;
+}
+
+/**
+ * Remove a leading http:// or https:// from a url.
+ * @param {string} url - the url from which to strip http(s)
+ * @returns {string} - the url with http(s):// removed
+ */
+export function removeHttps(url) {
+    return url.replace("http://", "").replace("https://", "");
+}
+
+/**
+ * Remove both url parameters and http(s):// from a url.
+ * @param {string} url - the url to strip
+ * @returns {string} - the url with parameters and http(s):// removed
+ */
+export function stripUrl(url) {
+    return removeHttps(removeUrlParams(url));
+}
+
+/**
+ * Check whether two urls match based only on host and path.
+ * @param {string} url1 - first url
+ * @param {string} url2 - second url to match against the first
+ * @returns {Boolean} - whether the two urls match on host and path
+ */
+export function approxMatchUrl(url1, url2) {
+    var url1Object = new URL(url1);
+    var url2Object = new URL(url2);
+    return (url1Object.hostname == url2Object.hostname &&
+            url1Object.pathname == url2Object.pathname);
+}
