d39adae677350e58ed29ea4a1c9cde3b14056c84
Pranay Anchuri <panchuri@princeton.edu>
2020-02-06 15:05:14 -0500

    (split)reducing footprint of utils

--- Studies/content-scripts/utils.js
+++ Studies/content-scripts/utils.js
@@ -1,40 +1,6 @@
 /**
  * Misc set of functions used in content scripts
- * @module WebScience.Studies.content-scripts.misc
  */
-
-/**
- * Convert relative url to abs url
- * @param {string} url 
- * @returns {string} absolute url
- */
-function relativeToAbsoluteUrl(url) {
-    /* Only accept commonly trusted protocols:
-     * Only data-image URLs are accepted, Exotic flavours (escaped slash,
-     * html-entitied characters) are not supported to keep the function fast */
-    if (/^(https?|file|ftps?|mailto|javascript|data:image\/[^;]{2,9};):/i.test(url))
-        return url; //Url is already absolute
-
-    var base_url = location.href.match(/^(.+)\/?(?:#.+)?$/)[0] + "/";
-    if (url.substring(0, 2) == "//")
-        return location.protocol + url;
-    else if (url.charAt(0) == "/")
-        return location.protocol + "//" + location.host + url;
-    else if (url.substring(0, 2) == "./")
-        url = "." + url;
-    else if (/^\s*$/.test(url))
-        return ""; //Empty = Return nothing
-    else url = "../" + url;
-
-    url = base_url + url;
-    var i = 0;
-    while (/\/\.\.\//.test(url = url.replace(/[^\/]+\/+\.\.\//g, "")));
-
-    /* Escape certain characters to prevent XSS */
-    url = url.replace(/\.$/, "").replace(/\/\./g, "").replace(/"/g, "%22")
-        .replace(/'/g, "%27").replace(/</g, "%3C").replace(/>/g, "%3E");
-    return url;
-}
 
 /**
  * Regular expression for matching urls shimmed by facebook
@@ -73,39 +39,4 @@ function removeShim(url) {
         return { url : removeFacebookShim(url), isShim : true};
     }
     return { url : url, isShim : false};
-}
-
-/**
- * Helper function to get size of element
- * @param {Element} element element
- * @returns Object with width and height of element
- */
-function getElementSize(element) {
-    let rect = element.getBoundingClientRect();
-    return {
-        width: rect.width,
-        height: rect.height
-    };
-}
-
-/**
- * Helper function to check if Element is visible based on style and bounding rectangle
- * @param {Element} element element
- * @returns {boolean} true if the element is visible
- */
-function isElementVisible(element) {
-    const rect = element.getBoundingClientRect();
-    const st = window.getComputedStyle(element);
-    let ret = (
-        element &&
-        element.offsetHeight > 0 &&
-        element.offsetWidth > 0 &&
-        rect &&
-        rect.height > 0 &&
-        rect.width > 0 &&
-        st &&
-        st.display && st.display !== "none" &&
-        st.opacity && st.opacity !== "0"
-    );
-    return ret;
 }
