966f8814da50c6b78bc36cd7a0fe1c0d892e76f9
Pranay Anchuri <panchuri@princeton.edu>
2020-02-06 22:55:27 -0500

    account exposure var rename

--- WebScience/Studies/content-scripts/socialMediaAccountExposure-youtube.js
+++ WebScience/Studies/content-scripts/socialMediaAccountExposure-youtube.js
@@ -19,22 +19,22 @@
         const waitMs = 2000;
         /** listener for new videos loaded; youtube doesn't reload page. It uses history api. */
         document.body.addEventListener("yt-navigate-finish", function (event) {
-            setTimeout(checkChannel, waitMs);
+            setTimeout(checkForVideosFromKnownChannels, waitMs);
         });
 
         /** sleep and then check for news video */
-        setTimeout(checkChannel, waitMs);
+        setTimeout(checkForVideosFromKnownChannels, waitMs);
         /**
          * @function
-         * @name checkChannel function checks if the current webpage has youtube watch/embed url
+         * @name checkForVideosFromKnownChannels function checks if the current webpage has youtube watch/embed url
          * for valid youtube videos, it checks if the video category is News & Politics
          * NOTE : if the inner html doesn't contain News & Politics, then the function
          * clicks on Show More and then checks DOM for video category
          */
-        function checkChannel() {
+        function checkForVideosFromKnownChannels() {
             let domLinkElements = Array.from(document.body.querySelectorAll("a[href]"));
             if (domLinkElements.length > 0) {
-                sendYoutubeChannelExposureEvent([...new Set(domLinkElements.filter(domLinkElement => knownMediaChannelMatcher.test(domLinkElement.href)).map(domLinkElement => {
+                sendMessage([...new Set(domLinkElements.filter(domLinkElement => knownMediaChannelMatcher.test(domLinkElement.href)).map(domLinkElement => {
                     return domLinkElement.href;
                 }))]);
             }
@@ -43,7 +43,7 @@
          * 
          * @param {Array} channels - channels exposed
          */
-        function sendYoutubeChannelExposureEvent(channels) {
+        function sendMessage(channels) {
             browser.runtime.sendMessage({
                 type: "WebScience.Studies.SocialMediaAccountExposure",
                 posts: [{
