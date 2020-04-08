3cf79531e139147d0427d29612f1654ce56ecffb
Pranay Anchuri <panchuri@princeton.edu>
2020-04-08 09:59:01 -0400

    adding documentation for analysis script

--- Measurements/Analysis.js
+++ Measurements/Analysis.js
@@ -1,4 +1,20 @@
+/**
+ * @file Sample worker script
+ */
+
+/**
+ * instance of indexedDB database
+ * @type {IDBDatabase} 
+ * @private
+ */
 let db;
+
+/**
+ * Event handler for messages from the main thread
+ * 
+ * @param {MessageEvent} event - message object
+ * @listens MessageEvent
+ */
 onmessage = event => {
     let data = event.data;
     switch(data) {
@@ -15,13 +31,27 @@ onmessage = event => {
     }
   }
 
+  /**
+   * Error handler
+   * @param {ErrorEvent} event - error object
+   * @listens ErrorEvent
+   */
+  onerror = event => {
+    console.error(event.message);
+  }
+
+  /**
+   * Sends messages to the main thread that spawned this worker thread.
+   * Each message has a type property for the main thread to handle messages.
+   * The data property in the message contains the data object that the worker
+   * thread intends to send to the main thread. 
+   * 
+   * @param {string} messageType message type
+   * @param {Object} data data to be sent
+   */
   function sendMessageToCaller(messageType, data) {
       postMessage({
           type: messageType,
           data: data
       });
-  }
-  
-  onerror = event => {
-    console.error(event.message)
   }
