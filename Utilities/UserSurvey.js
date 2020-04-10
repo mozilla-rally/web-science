aad1aa71163a5c8de5bc2a108113468f6c980b0e
Pranay Anchuri <panchuri@princeton.edu>
2020-04-10 00:15:05 -0400

    Adding documentation

--- Utilities/UserSurvey.js
+++ Utilities/UserSurvey.js
@@ -8,7 +8,20 @@ import * as Debugging from "./Debugging.js"
 
 var storage = null;
 
+/**
+ * Logger object
+ * 
+ * @constant
+ * @private
+ * @type {function(string)}
+ */
 const debugLog = Debugging.getDebuggingLog("Utilities.UserSurvey");
+
+/**
+ * The fully-qualified URL to Princeton shield image
+ * @constant
+ * @type {string}
+ */
 export const SHIELD_URL = browser.runtime.getURL("images/Princetonshieldlarge.png");
 
 browser.privileged.onSurveyPopup.addListener(async(url) => {
@@ -25,22 +38,30 @@ browser.privileged.onSurveyPopup.addListener(async(url) => {
     });
 });
 
-function scheduleSurvey(surveyUrl, surveyTime) {
+/**
+ * Schedule a survey popup using privileged API
+ * @param {string} surveyURLBase - Base URL for survey
+ * @param {number} surveyTime - When to create survey popup
+ */
+function scheduleSurvey(surveyURLBase, surveyTime) {
     browser.alarms.onAlarm.addListener(function () {
-        browser.privileged.createSurveyPopup(surveyUrl, surveyTime, SHIELD_URL);
+        browser.privileged.createSurveyPopup(surveyURLBase, surveyTime, SHIELD_URL);
     });
     browser.alarms.create(
         "surveyAlarm",
         { when: surveyTime }
     );
 }
+
 /**
- * Schedule a survey.
- * @param {string} surveyUrl - the website to send the user to
+ * Run a survey at scheduled survey time if it exists otherwise 
+ * current time + delta
+ * 
+ * @param {string} surveyURLBase - survey URL
  * @param {number} surveyTimeAfterInitialRun - amount of time to wait before presenting survey
  */
 export async function runStudy({
-    surveyUrl,
+    surveyURLBase,
     surveyTimeAfterInitialRun
 }) {
     storage = await(new Storage.KeyValueStorage("WebScience.Measurements.UserSurvey")).initialize();
@@ -48,13 +69,13 @@ export async function runStudy({
     // create listeners
     if (surveyTime) {
         if (surveyTime < Date.now()) {
-            scheduleSurvey(surveyUrl, surveyTime);
+            scheduleSurvey(surveyURLBase, surveyTime);
         } else {
-            browser.privileged.createSurveyPopup(surveyUrl, Date.now(), SHIELD_URL);
+            browser.privileged.createSurveyPopup(surveyURLBase, Date.now(), SHIELD_URL);
         }
     } else {
         surveyTime = surveyTimeAfterInitialRun + Date.now();
         await storage.set("surveyTime", surveyTime);
-        scheduleSurvey(surveyUrl, surveyTime);
+        scheduleSurvey(surveyURLBase, surveyTime);
     }
 }
