64b9cbe29ac5492a4241a340553fb6149445856c
Pranay Anchuri <panchuri@princeton.edu>
2020-04-23 15:15:09 -0400

    Refactoring stats computation to use init, compute, gather pattern

--- Measurements/AggregateStatistics.js
+++ Measurements/AggregateStatistics.js
@@ -23,34 +23,48 @@ onmessage = event => {
     sendMessageToCaller("stats ", stats);
 }
 
-  /**
-   * Error handler
-   * @param {ErrorEvent} event - error object
-   * @listens ErrorEvent
-   */
-  onerror = event => {
+/**
+ * Error handler
+ * @param {ErrorEvent} event - error object
+ * @listens ErrorEvent
+ */
+onerror = event => {
     console.error(event.message);
-  }
-
-  /**
-   * Sends messages to the main thread that spawned this worker thread.
-   * Each message has a type property for the main thread to handle messages.
-   * The data property in the message contains the data object that the worker
-   * thread intends to send to the main thread. 
-   * 
-   * @param {string} messageType message type
-   * @param {Object} data data to be sent
-   */
-  function sendMessageToCaller(messageType, data) {
-      postMessage({
-          type: messageType,
-          data: data
-      });
-  }
+}
 
 /**
-* Functions for computing statistics 
-*/
+ * Sends messages to the main thread that spawned this worker thread.
+ * Each message has a type property for the main thread to handle messages.
+ * The data property in the message contains the data object that the worker
+ * thread intends to send to the main thread. 
+ * 
+ * @param {string} messageType message type
+ * @param {Object} data data to be sent
+ */
+function sendMessageToCaller(messageType, data) {
+    postMessage({
+        type: messageType,
+        data: data
+    });
+}
+
+function StorageStatistics(setup, compute, gather) {
+    this.setup = setup;
+    this.compute = compute;
+    this.gather = gather;
+}
+
+StorageStatistics.prototype.computeStats = function (storageInstance) {
+    let stats = this.setup();
+    Object.entries(storageInstance).forEach(entry => {
+        this.compute(entry, stats);
+    });
+    return this.gather(stats);
+}
+
+/**
+ * Functions for computing statistics 
+ */
 
 /**
  * The number of seconds in a day.
@@ -79,8 +93,153 @@ const _MAX_DATE = 8640000000000000;
 const functionMapping = {
     "WebScience.Measurements.SocialMediaAccountExposure": socialMediaAccountExposureStats,
     "WebScience.Measurements.SocialMediaNewsExposure": socialMediaNewsExposureStats,
-    "WebScience.Measurements.PageNaivgation": pageNavigationStats,
+    "WebScience.Measurements.PageNavigation": pageNavigationStats,
     "WebScience.Measurements.LinkExposure": linkExposureStats
+}
+
+/**
+ * Given an object containing aggregate statistics, the function unwraps the
+ * proxy object, copies the values and revokes proxy.
+ * @param {Object} statsObj statistics object
+ */
+function gatherStats(statsObj) {
+    let overallStats = {};
+    Object.entries(statsObj).forEach(entry => {
+        overallStats[entry[0]] = copyDefaultValueObject(entry[1].proxy);
+        entry[1].revoke();
+    });
+    return overallStats;
+}
+
+/**
+ * Function for computing page navigation statistics
+ * @param {Object} pageNavigationStorage page navigation storage object
+ */
+function pageNavigationStats(pageNavigationStorage) {
+    let statsObj = new StorageStatistics(
+        () => {
+            let stats = {};
+            stats.attention_duration = objectWithDefaultValue();
+            stats.page_domain = objectWithDefaultValue(); // key is domain and value is number of pages in that domain
+            stats.page_url = objectWithDefaultValue(); // key is url and value is number unique visits, multiple attentions are counted as a single visit
+            stats.referrer_url = objectWithDefaultValue(function () {
+                return new Set();
+            }); // key is referrer url and value is number of unique urls it referred
+            return stats;
+        },
+        (entry, stats) => {
+            let navObj = entry[1];
+            let domain = getDomain(navObj.url);
+            stats.page_domain.proxy[domain] += 1;
+            stats.page_url.proxy[navObj.url] += 1;
+            stats.referrer_url.proxy[navObj.referrer] = stats.referrer_url.proxy[navObj.referrer].add(navObj.url);
+            let attention = 0;
+            navObj.attentionSpanStarts.map((start_time, index) => {
+                let end_time = navObj.attentionSpanEnds[index];
+                attention += end_time - start_time;
+            });
+            stats.attention_duration.proxy[navObj.url] += attention
+        },
+        gatherStats
+    );
+    return statsObj.computeStats(pageNavigationStorage);
+
+}
+
+/**
+ * Function for computing link exposure statistics
+ * @param {Object} linkExposureStorage page navigation storage object
+ */
+function linkExposureStats(linkExposureStorage) {
+    let statsObj = new StorageStatistics(
+        () => {
+    let stats = {};
+    stats.source_domains = objectWithDefaultValue();
+    stats.source_domains_category = objectWithDefaultValue();
+    stats.source_urls = objectWithDefaultValue();
+    stats.exposed_domains = objectWithDefaultValue();
+    stats.exposed_urls = objectWithDefaultValue();
+    stats.source_first_seen = objectWithDefaultValue(() => {
+        return new Date(_MAX_DATE);
+    });
+    return stats;
+
+        },
+        (entry, stats) => {
+        let val = entry[1];
+        if ('metadata' in val) {
+            if ('location' in val.metadata) {
+                let source_domain = getDomain(val.metadata.location);
+                stats.source_domains.proxy[source_domain] += 1;
+                stats.source_urls.proxy[val.metadata.location] += 1;
+            }
+            if ('domainCategory' in val.metadata) {
+                stats.source_domains_category.proxy[val.metadata.domainCategory] += 1;
+            }
+            if ('loadTime' in val.metadata) {
+                stats.source_first_seen.proxy[val.metadata.location] = Math.min(stats.source_first_seen.proxy[val.metadata.location], new Date(val.metadata.loadTime));
+            }
+        }
+        let exposedURL = val.resolvedUrl ? 'resolvedUrl' in val : val.originalUrl;
+        let exposedDomain = getDomain(exposedURL);
+        stats.exposed_domains.proxy[exposedDomain] += 1;
+        stats.exposed_urls.proxy[exposedURL] += 1;
+
+        },
+        gatherStats
+    );
+    return statsObj.computeStats(linkExposureStorage);
+}
+
+
+/**
+ * Function for computing social media account exposure statistics
+ * @param {Object} socialMediaAccountExposureStorage social media account exposure storage
+ */
+function socialMediaAccountExposureStats(socialMediaAccountExposureStorage) {
+    let statsObj = new StorageStatistics(
+        () => {
+            let stats = {};
+            stats.account_posts = objectWithDefaultValue(function () {
+                return {
+                    platform: "",
+                    count: 0
+                }
+            });
+            return stats;
+        },
+        (entry, stats) => {
+            let val = entry[1];
+            val.posts.forEach(post => {
+                stats.account_posts.proxy[post.account] = {
+                    platform: val.platform,
+                    count: stats.account_posts.proxy[post.account].count + 1
+                }
+            });
+        },
+        gatherStats
+    );
+    return statsObj.computeStats(socialMediaAccountExposureStorage);
+}
+
+/**
+ * Function for computing social media news exposure statistics
+ * @param {Object} socialMediaNewsExposureStorage social media news exposure storage
+ */
+function socialMediaNewsExposureStats(socialMediaNewsExposureStorage) {
+    let statsObj = new StorageStatistics(
+        () => {
+            let stats = {};
+            stats.source_counts = objectWithDefaultValue();
+            return stats;
+        },
+        (entry, stats) => {
+            let val = entry[1];
+            stats.source_counts.proxy[val.type] += 1;
+        },
+        gatherStats
+    );
+    return statsObj.computeStats(socialMediaNewsExposureStorage);
 }
 
 /**
@@ -123,9 +282,11 @@ function getDomain(url) {
  * function instead of undefined value.
  * @param {function} defaultValue - Function that returns a default value
  */
-function objectWithDefaultValue(defaultValue = () => {return 0}) {
+function objectWithDefaultValue(defaultValue = () => {
+    return 0
+}) {
     return Proxy.revocable({}, {
-        get : (obj, property) => (property in obj) ? obj[property] : defaultValue()
+        get: (obj, property) => (property in obj) ? obj[property] : defaultValue()
     })
 }
 
@@ -139,117 +300,4 @@ function copyDefaultValueObject(defaultValueObject) {
         ret[entry[0]] = entry[1];
     })
     return ret;
-}
-
-/**
- * Given an object containing aggregate statistics, the function unwraps the
- * proxy object, copies the values and revokes proxy.
- * @param {Object} statsObj statistics object
- */
-function gatherStats(statsObj) {
-    let overallStats = {};
-    Object.entries(statsObj).forEach(entry => {
-        overallStats[entry[0]] = copyDefaultValueObject(entry[1].proxy);
-        entry[1].revoke();
-    });
-    return overallStats;
-}
-
-/**
- * Function for computing page navigation statistics
- * @param {Object} pageNavigationStorage page navigation storage object
- */
-function pageNavigationStats(pageNavigationStorage) {
-    let stats = {};
-    stats.attention_duration = objectWithDefaultValue();
-    stats.page_domain = objectWithDefaultValue(); // key is domain and value is number of pages in that domain
-    stats.page_url = objectWithDefaultValue(); // key is url and value is number unique visits, multiple attentions are counted as a single visit
-    stats.referrer_url = objectWithDefaultValue(function() {return new Set();}); // key is referrer url and value is number of unique urls it referred
-    Object.entries(pageNavigationStorage).forEach(entry => {
-
-        let key = entry[0];
-        let navObj = entry[1];
-        let domain = getDomain(navObj.url);
-        stats.page_domain.proxy[domain] += 1;
-        stats.page_url.proxy[navObj.url] += 1;
-        stats.referrer_url.proxy[navObj.referrer] = stats.referrer_url.proxy[navObj.referrer].add(navObj.url);
-        let attention = 0;
-        navObj.attentionSpanStarts.map((start_time, index) => {
-            let end_time = navObj.attentionSpanEnds[index];
-            attention += end_time - start_time;
-        });
-        stats.attention_duration.proxy[navObj.url] += attention
-    });
-    return gatherStats(stats);
-}
-
-/**
- * Function for computing link exposure statistics
- * @param {Object} linkExposureStorage page navigation storage object
- */
-function linkExposureStats(linkExposureStorage) {
-    let stats = {};
-    stats.source_domains = objectWithDefaultValue();
-    stats.source_domains_category = objectWithDefaultValue();
-    stats.source_urls = objectWithDefaultValue();
-    stats.exposed_domains = objectWithDefaultValue();
-    stats.exposed_urls = objectWithDefaultValue();
-    stats.source_first_seen = objectWithDefaultValue(() => { return new Date(_MAX_DATE); });
-    Object.keys(linkExposureStorage).forEach(key => {
-        let val = linkExposureStorage[key];
-        if ('metadata' in val) {
-            if ('location' in val.metadata) {
-                let source_domain = getDomain(val.metadata.location);
-                stats.source_domains.proxy[source_domain] += 1;
-                stats.source_urls.proxy[val.metadata.location] += 1;
-            }
-            if ('domainCategory' in val.metadata) {
-                stats.source_domains_category.proxy[val.metadata.domainCategory] += 1;
-            }
-            if ('loadTime' in val.metadata) {
-                stats.source_first_seen.proxy[val.metadata.location] = Math.min(stats.source_first_seen.proxy[val.metadata.location], new Date(val.metadata.loadTime));
-            }
-        }
-        let exposedURL = val.resolvedUrl ? 'resolvedUrl' in val : val.originalUrl;
-        let exposedDomain = getDomain(exposedURL);
-        stats.exposed_domains.proxy[exposedDomain] += 1;
-        stats.exposed_urls.proxy[exposedURL] += 1;
-    });
-    return gatherStats(stats);
-}
-
-
-/**
- * Function for computing social media account exposure statistics
- * @param {Object} socialMediaAccountExposureStorage social media account exposure storage
- */
-function socialMediaAccountExposureStats(socialMediaAccountExposureStorage) {
-    let stats = {};
-    stats.account_posts = objectWithDefaultValue(function () {
-        return { platform: "", count: 0 };
-    }); // key is account and value is number of exposed posts
-    Object.entries(socialMediaAccountExposureStorage).forEach(entry => {
-        let val = entry[1];
-        val.posts.forEach(post => {
-            stats.account_posts.proxy[post.account] = {
-                platform: val.platform,
-                count: stats.account_posts.proxy[post.account].count + 1
-            }
-        })
-    });
-    return gatherStats(stats);
-}
-
-/**
- * Function for computing social media news exposure statistics
- * @param {Object} socialMediaNewsExposureStorage social media news exposure storage
- */
-function socialMediaNewsExposureStats(socialMediaNewsExposureStorage) {
-    let stats = {};
-    stats.source_counts = objectWithDefaultValue();
-    Object.entries(socialMediaNewsExposureStorage).forEach(entry => {
-        let val = entry[1];
-        stats.source_counts.proxy[val.type] += 1;
-    })
-    return gatherStats(stats);
 }
