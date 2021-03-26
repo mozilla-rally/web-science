8bfa003e8b5141d104eacf338f5db696eb2bf167
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2021-03-26 18:40:28 -0400

    Streamlined second build step

--- rollup.config.final.js
+++ rollup.config.final.js
@@ -4,34 +4,25 @@
  *   * Convert content script files to background script files with inlined content scripts.
  */
 
-import globby from "globby";
 import url from "@rollup/plugin-url";
 
 const intermediateDirectory = "intermediate";
 const distributionDirectory = "dist";
 
 export default (cliArgs) => {
-  const rollupConfig = [];
-  const backgroundScriptPaths = globby.sync([
-    `${intermediateDirectory}/**/*.js`,
-    `!${intermediateDirectory}/**/*.content.js`
-  ]);
-  for(const backgroundScriptPath of backgroundScriptPaths) {
-    rollupConfig.push({
-      input: backgroundScriptPath,
-      output: {
-        dir: distributionDirectory,
-        preserveModules: true,
-        preserveModulesRoot: intermediateDirectory,
-        format: "es"
-      },
-      plugins: [
-        url({
-          include: "**/*.content.js",
-          limit: Number.MAX_VALUE // Inline regardless of content script size
-        })
-      ]
-    });
-  }
-  return rollupConfig;
+  return [{
+    input: `${intermediateDirectory}/WebScience.js`,
+    output: {
+      dir: distributionDirectory,
+      preserveModules: true,
+      preserveModulesRoot: intermediateDirectory,
+      format: "es"
+    },
+    plugins: [
+      url({
+        include: "**/*.content.js",
+        limit: Number.MAX_VALUE // Inline regardless of content script size
+      })
+    ]
+  }];
 }
