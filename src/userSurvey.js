06e44f40d7ae708ba61f67e4f6b843c596786700
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2021-04-04 16:22:51 -0400

    Removing study-specific code from userSurvey #10

--- src/userSurvey.js
+++ src/userSurvey.js
@@ -1,164 +1,201 @@
 /**
- * Present surveys to the user.
+ * A module to facilitate surveys of study participants.
+ * 
+ * # User Experience
+ *   * If the user has not been previously prompted for the survey,
+ *     the survey will open in a new tab.
+ *   * The study's browser action popup will contain either a page
+ *     prompting the user complete the survey (with options to open
+ *     the survey or decline the survey), or a neutral page (if the
+ *     user has already completed or declined the survey).
+ *   * If the user has been previously prompted for the survey, and
+ *     has not completed or declined the survey, the user will be
+ *     reminded to complete the survey with a browser notification
+ *     at a set interval.
+ * 
+ * # Limitations
+ * Note that this module is currently very limited: it only supports
+ * one survey per study, with few options and a constrained design.
+ * We have not yet decided whether to build out this module or implement
+ * survey functionality in the Rally core add-on.
  * @module webScience.userSurvey
  */
-import * as storage from "./storage.js"
-import * as messaging from "./messaging.js"
+import * as id from "./id.js";
+import * as storage from "./storage.js";
+import * as messaging from "./messaging.js";
 
+/**
+ * @type {storage.KeyValueStorage}
+ * A persistent storage space for data about surveys.
+ */
 let storageSpace = null;
 
 /**
- * The fully-qualified URL to Princeton shield image
- * @constant
- * @type {string}
+ * @type {boolean}
+ * Whether a survey has already been created. This module
+ * currently only supports one survey per study.
  */
-const SHIELD_URL = browser.runtime.getURL("images/Princetonshieldlarge.png");
-// TODO -- wording, icon image
-const surveyRequestMessage = "A new survey is available for your Ion study. Click the Princeton logo in the toolbar to continue.";
-const surveyRequestTitle = "New Ion survey available";
+let createdSurvey = false;
 
-const secondsPerDay = 86400;
+// Module-wide variables for a survey, set in createSurvey
+let lastSurveyRequest = 0;
+let popupNoPromptPage = "";
+let reminderIconUrl = "";
+let reminderInterval = 0;
+let reminderMessage = "";
+let reminderTitle = "";
+let surveyUrl = "";
 
 const millisecondsPerSecond = 1000;
 
-const surveyRemindPeriodDays = 3;
-
-const surveyCompletionUrl = "https://citpsurveys.cs.princeton.edu/thankyou";
-
-let surveyUrlBase = "";
-
 /**
- * Generates a RFC4122 compliant ID
- * https://www.ietf.org/rfc/rfc4122.txt based on given seed.
- *
- * A compliant UUID is of the form
- * xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx; where 1 <= M <= 5
- * In this implementation M = 4.
- *
- * @param  {number} seed - seed. Example UTC milliseconds
- * @returns {string} - UUID
+ * Opens the survey URL in a new browser tab, appending parameters
+ * for the participant's survey ID (surveyID) and timezone offset
+ * (timezone).
  */
-function generateUUID(seed) {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        let r = Math.random() * 16;
-        r = (seed + r) % 16 | 0;
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
-}
-
-async function openSurveyTab(useSameTab = false) {
-    const surveyId = await storageSpace.get("surveyId");
-    const surveyUrlFull = surveyUrlBase +
-        "?surveyId=" + surveyId +
-        "&timezone=" + new Date().getTimezoneOffset();
-    if (useSameTab) {
-        browser.tabs.update({url: surveyUrlFull});
-        return;
-    }
+async function openSurveyInNewTab() {
+    const surveyId = await getSurveyId();
+    const surveyUrlObj = new URL(surveyUrl);
+    surveyUrlObj.searchParams.append("surveyId", surveyId);
+    surveyUrlObj.searchParams.append("timezone", new Date().getTimezoneOffset());
     browser.tabs.create({
         active: true,
-        url: surveyUrlFull
+        url: surveyUrlObj.href
     });
-}
-
-async function requestSurvey(alarm) {
-    if (alarm.name == "surveyAlarm") {
-        const surveyCompleted = await storageSpace.get("surveyCompleted");
-        const noRequestSurvey = await storageSpace.get("noRequestSurvey");
-        if (surveyCompleted) return;
-        if (noRequestSurvey) return;
-        const currentTime = Date.now();
-        await storageSpace.set("lastSurveyRequest", currentTime);
-        browser.notifications.create({
-            type: "image",
-            message: surveyRequestMessage,
-            title: surveyRequestTitle,
-            iconUrl: SHIELD_URL
-        });
-        scheduleSurveyRequest(currentTime);
-    }
-}
-
-function scheduleSurveyRequest(lastSurveyRequest) {
-    browser.alarms.onAlarm.addListener(requestSurvey);
-    browser.alarms.create("surveyAlarm", {
-        when: lastSurveyRequest + (millisecondsPerSecond * secondsPerDay * surveyRemindPeriodDays)
-    });
-}
-
-function handleSurveyCompleted() {
-    storageSpace.set("surveyCompleted", true);
-    setPopupSurveyCompleted();
-}
-
-function setPopupSurveyCompleted() {
-    browser.browserAction.setPopup({
-        popup: browser.runtime.getURL("study/completedSurvey.html")
-    });
-}
-
-function cancelSurveyRequest() {
-    storageSpace.set("noRequestSurvey", true);
-    setPopupSurveyCompleted();
 }
 
 /**
- * Run a survey at scheduled survey time if it exists otherwise
- * current time + delta
- *
- * @param {string} surveyUrl - survey URL
+ * Set a timeout to remind the user to complete the study.
  */
-export async function runStudy({
-    surveyUrl
-}) {
+function scheduleReminderForUser() {
+    setTimeout(remindUser, Math.max((lastSurveyRequest + reminderInterval * millisecondsPerSecond) - Date.now(), 0));
+}
+
+/**
+ * Remind the user to complete the study, by prompting with a notification.
+ */
+async function remindUser() {
+    const surveyCompleted = await storageSpace.get("surveyCompleted");
+    const surveyCancelled = await storageSpace.get("surveyCancelled");
+    if (surveyCompleted || surveyCancelled) {
+        return;
+    }
     const currentTime = Date.now();
-    surveyUrlBase = surveyUrl;
+    await storageSpace.set("lastSurveyRequest", currentTime);
+    browser.notifications.create({
+        type: "image",
+        message: reminderMessage,
+        title: reminderTitle,
+        iconUrl: reminderIconUrl
+    });
+    scheduleReminderForUser();
+}
+
+/**
+ * Set the browser action popup to the survey's no prompt page.
+ */
+function setPopupToNoPromptPage() {
+    browser.browserAction.setPopup({
+        popup: browser.runtime.getURL(popupNoPromptPage)
+    });
+}
+
+/**
+ * Prompt the user to respond to a survey.
+ * @param {Object} options - The options for the survey.
+ * @param {string} options.popupNoPromptPage - A path to an HTML file, relative
+ * to the study extension's root, to use in a browser action popup when the user
+ * is not being prompted to complete the survey (i.e., the survey is complete or
+ * cancelled).
+ * @param {string} options.popupPromptPage - A path to an HTML file, relative
+ * to the study extension's root, to use in a browser action prompting
+ * the user to complete the survey.
+ * @param {string} options.reminderIcon - A path to an icon file, relative
+ * to the study extension's root, to use for for reminding the user with a
+ * notification to complete the survey.
+ * @param {number} options.reminderInterval - How often, in seconds, to wait before
+ * reminding the user with a notification to participate in the survey.
+ * @param {string} options.reminderMessage - The message to use for reminding the
+ * user with a notification to complete the survey.
+ * @param {string} options.reminderTitle - The title to use for reminding the
+ * user with a notification to complete the survey.
+ * @param {string} options.surveyCompletionUrl - A URL that, when loaded,
+ * indicates the user has completed the survey.
+ * @param {string} options.surveyUrl - The URL for the survey on an external
+ * platform (e.g., SurveyMonkey, Typeform, Qualtrics, etc.).
+ */
+export async function createSurvey(options) {
+    if(createdSurvey) {
+        throw new Error("userSurvey only supports one survey at present.");
+    }
+    createdSurvey = true;
+
+    const currentTime = Date.now();
+    surveyUrl = options.surveyUrl;
+    reminderIconUrl = browser.runtime.getURL(options.reminderIcon);
+    reminderInterval = options.reminderInterval;
+    reminderTitle = options.reminderTitle;
+    reminderMessage = options.reminderMessage;
+    popupNoPromptPage = options.popupNoPromptPage;
 
     storageSpace = new storage.KeyValueStorage("webScience.userSurvey");
-    /* Check when we last asked the user to do the survey. If it's null,
-     * we've never asked, which means the extension just got installed.
-     * Open a tab with the survey, and save this time as the most recent
-     * request for participation.
-     */
-    let lastSurveyRequest = await storageSpace.get("lastSurveyRequest");
+
+    // Check when we last asked the user to complete the survey. If the value
+    // is null, we've never asked, which means the extension just got installed.
+    // Open a tab with the survey, and save this time as the most recent
+    // request for participation.
     const surveyCompleted = await storageSpace.get("surveyCompleted");
-    const noRequestSurvey = await storageSpace.get("noRequestSurvey");
-    if (surveyCompleted || noRequestSurvey) {
-        setPopupSurveyCompleted();
-        await storageSpace.set("surveyCompleted", surveyCompleted);
+    const surveyCancelled = await storageSpace.get("surveyCancelled");
+    lastSurveyRequest = await storageSpace.get("lastSurveyRequest");
+
+    // Configure the browser action popup page
+    if (surveyCompleted || surveyCancelled) {
+        setPopupToNoPromptPage();
         return;
-    } else {
+    }
+    else {
         browser.browserAction.setPopup({
-            popup: browser.runtime.getURL("study/survey.html")
+            popup: browser.runtime.getURL(options.popupPromptPage)
         });
     }
 
-    // null means we've never asked before, so the extension was just enabled
-    if (lastSurveyRequest == null) {
+    // If this is the first survey request, open the survey in a new tab
+    if (lastSurveyRequest === null) {
         lastSurveyRequest = currentTime;
         await storageSpace.set("lastSurveyRequest", lastSurveyRequest);
         await storageSpace.set("surveyCompleted", false);
-        await storageSpace.set("noRequestSurvey", false);
-        await storageSpace.set("surveyId", generateUUID(Date.now()));
-        openSurveyTab(false);
+        await storageSpace.set("surveyCancelled", false);
+        await storageSpace.set("surveyId", id.generateId());
+        openSurveyInNewTab();
     }
-    /* Set a time to ask the user in three days (we won't actually ask
-     * if they end up completing the survey before then).
-     */
-    scheduleSurveyRequest(lastSurveyRequest);
-    /* The user gets directed to this page at the end of the survey, so
-     * seeing a request for it means they did it -- stop bothering them
-     */
+
+    // Schedule a reminder for the user
+    scheduleReminderForUser();
+
+    // Set a listener for the survey completion URL
     browser.webRequest.onBeforeRequest.addListener(
-        handleSurveyCompleted,
-        {urls: [ surveyCompletionUrl + "*" ]}
+        () => {
+            storageSpace.set("surveyCompleted", true);
+            setPopupToNoPromptPage();
+        },
+        { urls: [ (new URL(options.surveyCompletionUrl)).href + "*" ] }
     );
 
-    /* If the user tells us to never ask them again, we catch it with this message */
-    messaging.registerListener("webScience.userSurvey.cancelSurveyRequest", cancelSurveyRequest);
-    messaging.registerListener("webScience.userSurvey.openSurveyTab", () => { openSurveyTab(false); });
+    // Set listeners for cancel and open survey button clicks in the survey request
+    messaging.registerListener("webScience.userSurvey.cancelSurvey", () => {
+        storageSpace.set("surveyCancelled", true);
+        setPopupToNoPromptPage();
+    });
+    messaging.registerListener("webScience.userSurvey.openSurvey", openSurveyInNewTab);
 }
 
+/**
+ * Each study participant has a persistent survey ID, generated with
+ * the id module. The ID is automatically added as a parameter to
+ * the survey URL, enabling researchers to import survey data from an
+ * external platform and sync it with Rally data.
+ * @returns {string} - The participant's survey ID.
+ */
 export async function getSurveyId() {
     return await storageSpace.get("surveyId");
 }
