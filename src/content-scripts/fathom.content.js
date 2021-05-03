45d88bd7020ad4353dbd1a8960caad057b33972b
Victor Ongkowijaya <victwj@princeton.edu>
2021-05-03 15:54:05 -0400

    Add fathom content script

---
+++ src/content-scripts/fathom.content.js
@@ -0,0 +1,67 @@
+/**
+ * Content script for the Fathom module. Uses Mozilla Fathom
+ * to classify the DOM elements of a page.
+ * @module webScience.fathom.content
+ */
+
+(function() {
+    // Check if content script is already running
+    if ("webScience" in window) {
+        if ("fathomActive" in window.webScience) {
+            return;
+        }
+        window.webScience.fathomActive = true;
+    }
+    else {
+        // Else, this is the first webScience initialization
+        window.webScience = {
+            fathomActive: true
+        }
+    }
+
+    /**
+     * Whether the elements of this page have been classified.
+     * @type {boolean}
+     */
+    let pageClassified = false;
+    
+    // Function to call once pageManager is loaded
+    function pageManagerLoaded() {
+        console.log("pageManager loaded, running fathom content script");
+        const pageManager = window.webScience.pageManager;
+
+        // Listen for messages from background script
+        // Background script will tell us if this page should be classified
+        // and what Fathom rulesets to use
+        browser.runtime.onMessage.addListener((message) => {
+            if (message.type !== "webScience.fathom.isClassifiable" ||
+                !message.isClassifiable) {
+                return;
+            }
+
+            // run Fathom here
+            pageClassified = true;
+
+            // Send results to background script
+            pageManager.sendMessage({
+                type: "webScience.fathom.fathomData",
+                test: "hello from content script"
+            });
+        });
+    }
+
+    // If pageManager is loaded, call our main function
+    if ("webScience" in window && "pageManager" in window.webScience) {
+        pageManagerLoaded();
+    }
+    // If pageManager is not loaded, push our main function to queue
+    else {
+        if (!("pageManagerHasLoaded" in window)) {
+            window.pageManagerHasLoaded = [];
+        }
+        window.pageManagerHasLoaded.push(pageManagerLoaded());
+    }
+
+    console.log("content script initialized");
+})();
+
