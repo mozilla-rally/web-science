ac55adcb31dcd8f516d430d4bf65522f94a2c581
Jonathan Mayer <382615+jonathanmayer@users.noreply.github.com>
2021-06-05 16:02:50 -0400

    Adding Rollup plugin template #101

---
+++ rollup-plugin.js
@@ -0,0 +1,11 @@
+export default function webScienceRollupPlugin() {
+    return {
+        name: "webscience-rollup-plugin",
+        resolveId(source) {
+            return null;
+        },
+        load(id) {
+            return null;
+        }
+    };
+}
