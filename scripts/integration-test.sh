bf783368be707c81db34e573dc7448cc97f33360
Robert Helmer <rhelmer@rhelmer.org>
2022-04-19 18:17:43 -0700

    run Chrome non-headless with xvfb-run in CI

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -10,8 +10,8 @@ npm install ../../../
 npm install && npm run build && npm run package
 popd
 
-echo "Testing Firefox with extension"
+echo "Testing Firefox headless with extension"
 npm run test:integration:jest -- --test_browser=firefox --load_extension=true --headless_mode=true  2>&1 | tee integration.log
-# FIXME Chrome Headless mode does not support extensions, need to set up a display server if we want this to work.
-# echo "Testing Chrome with extension"
-# npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false 2>&1 | tee integration.log
+# NOTE Chrome Headless mode does not support extensions, so we use `xvfb` as the display server.
+echo "Testing Chrome non-headless with extension"
+xvfb-run npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false 2>&1 | tee integration.log
