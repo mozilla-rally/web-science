2dba05d99ed293638245a937dbd19cbafeff0c1c
Robert Helmer <rhelmer@rhelmer.org>
2022-04-19 18:38:47 -0700

    detect xvfb-run and warn if not present

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
+XVFB_RUN() {
+    if hash xvfb-run 2>/dev/null
+    then
+        xvfb-run "$@"
+    else
+        echo "WARN: xvfb-run must be installed to run Chrome with extensions enabled on a headless server"
+        "$@"
+    fi
+}
+
 export PATH=$PATH:./node_modules/.bin
 
 echo "Building test extension..."
@@ -14,4 +24,4 @@ echo "Testing Firefox headless with extension"
 npm run test:integration:jest -- --test_browser=firefox --load_extension=true --headless_mode=true  2>&1 | tee integration.log
 # NOTE Chrome Headless mode does not support extensions, so we use `xvfb` as the display server.
 echo "Testing Chrome non-headless with extension"
-xvfb-run npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false 2>&1 | tee integration.log
+XVFB_RUN npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false 2>&1 | tee integration.log
