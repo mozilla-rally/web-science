a7b9a7ba6c5d930ebd69cdf8d1826cd103f634d2
Robert Helmer <rhelmer@mozilla.com>
2022-07-28 10:53:42 -0700

    build mv2 for Firefox, mv3 for Chrome before integration tests

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -13,15 +13,21 @@ XVFB_RUN() {
 
 export PATH=$PATH:./node_modules/.bin
 
-echo "Building test extension..."
+echo "Building test extension for manifest v2..."
 pwd
 pushd tests/integration/extension
 npm install ../../../
-npm install && npm run build && npm run package
+npm install && npm run build && npm run package:mv2
 popd
+echo "Testing Firefox headless with extension"
+npm run test:integration:jest -- --test_browser=firefox --load_extension=true --headless_mode=true  2>&1 | tee integration.log
 
-#echo "Testing Firefox headless with extension"
-#npm run test:integration:jest -- --test_browser=firefox --load_extension=true --headless_mode=true  2>&1 | tee integration.log
+echo "Building test extension for manifest v3..."
+pwd
+pushd tests/integration/extension
+npm install ../../../
+npm install && npm run build && npm run package:mv3
+popd
 # NOTE Chrome Headless mode does not support extensions, so we use `xvfb` as the display server.
 echo "Testing Chrome non-headless with extension"
 XVFB_RUN npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false 2>&1 | tee integration.log
